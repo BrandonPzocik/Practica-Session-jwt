70b120ab0bb7172c7c5c392f180f0d441e5bd293
Brandon <pzocikbrandon@gmail.com>
2024-09-05 15:23:02 -0300

    se crearon y terminaron las rutas

---
+++ backend-jwt/routes/auth.routes.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import validarJwt from "../middlewares/validar-jwt.js";
+import {
+  loginCtrl,
+  logOutCtrl,
+  sessionCtrl,
+} from "../controllers/auth.controllers.js";
+
+const authRouter = Router();
+
+// Endpoint de inicio de sesión (login)
+authRouter.post("/login", loginCtrl);
+
+// Endpoint para validar la sesión
+authRouter.get("/session", validarJwt, sessionCtrl);
+
+// Endpoint de cierre de sesión (logout)
+authRouter.post("/logout", logOutCtrl);
+
+export { authRouter };
